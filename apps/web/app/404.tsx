98b9a9e6f609d14b7036ac61d481d2479c2b4302
Shane Jonas <jonas.shane@gmail.com>
2024-11-22 13:33:08 -0500

    fix: build + dedupe react

---
+++ apps/web/app/404.tsx
@@ -0,0 +1,11 @@
+'use client'
+ 
+export default function NotFound() {
+  return (
+    <div>
+      <h2>Not Found</h2>
+      <p>Could not find requested resource</p>
+      <a href="/">Return Home</a>
+    </div>
+  )
+}
---
+++ apps/web/app/500.tsx
@@ -0,0 +1,10 @@
+'use client'
+ 
+export default function InternalServerError() {
+  return (
+    <div>
+      <h2>Internal Server Error</h2>
+      <a href="/">Return Home</a>
+    </div>
+  )
+}
