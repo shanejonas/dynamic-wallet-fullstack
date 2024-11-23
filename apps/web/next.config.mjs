5fd9accfd0cf78120bd65622ec227c143894c249
Shane Jonas <jonas.shane@gmail.com>
2024-11-22 19:47:18 -0500

    fix: next config basepath

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    basePath: process.env.CI ? '/dynamic-wallet-fullstack' : '',
 };
 
 export default nextConfig;
