df66444b5def2afe533e030cc43c429018a530ed
Shane Jonas <jonas.shane@gmail.com>
2024-11-22 19:35:14 -0500

    fix: add deploy and env variables

--- apps/web/src/lib/jsonrpc-client.ts
+++ apps/web/src/lib/jsonrpc-client.ts
@@ -1,5 +1,6 @@
 import { RequestManager, HTTPTransport, Client } from "@open-rpc/client-js";
-let transport = new HTTPTransport("http://localhost:8080/rpc/v1");
+const URL = process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:8080/rpc/v1";
+let transport = new HTTPTransport(URL);
 let client = new Client(new RequestManager([transport]));
 
 export const getClient = () => {
@@ -8,7 +9,7 @@ export const getClient = () => {
 
 export const setAuthorizationHeader = (header: string) => {
     // header = header.replace(/^["']|["']$/g, ''); // TODO: remove this
-    client = new Client(new RequestManager([new HTTPTransport("http://localhost:8080/rpc/v1", {
+    client = new Client(new RequestManager([new HTTPTransport(URL, {
         headers: {
             Authorization: "Bearer " + header,
         },
